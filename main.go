1c812d35c0fc26295be1f3925790c5ffb59bda69
Veligorro <31781336+Veligorro@users.noreply.github.com>
2022-08-11 15:01:49 +0300

    Update main.go

--- main.go
+++ main.go
@@ -31,41 +31,6 @@ type Signatures struct {
 	Signatures      []string
 }
 
-// мой вариант хендлера на основе echo, а не пакета http
-// предложенный в учебнике вариант через пакет template не работает должным образом
-// содержимое файла в формате html переносим в байтовый срез, который используем как аргумент для метода HTML
-
-// хорошая функция для работы с обычным html без шаблонов
-//func newHandler(c echo.Context) error {
-//	f, err := os.Open("new.html")
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	defer f.Close()
-//	wr := bytes.Buffer{} //
-//	sc := bufio.NewScanner(f)
-//	for sc.Scan() {
-//		wr.WriteString(sc.Text())
-//	}
-//	return c.HTML(http.StatusOK, wr.String())
-//}
-
-/* на удивление рабочий вариант на простых элементах, без использования пакета bytes
-func testHandler(c echo.Context) error {
-	f, err := os.Open("new.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-	var wr string
-	sc := bufio.NewScanner(f)
-	for sc.Scan() {
-		wr += sc.Text()
-	}
-	return c.HTML(http.StatusOK, wr)
-}
- делалось ради проверки */
-//
 
 func getString(fileName string) []string {
 	var lines []string
